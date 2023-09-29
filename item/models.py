920149f6e03b8a31e8f5b732d137a35a0a3e2736
pseudoclone <prince.077bei027@acem.edu.np>
2023-09-29 08:45:57 +0545

    update: setup foreign key for items -> Users

--- item/models.py
+++ item/models.py
@@ -11,3 +11,15 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True) #If user enters nothing in the description, then its fine too
+    price = models.FloatField()
+    isSold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User,related_name = 'items', on_delete = models.CASCADE)
+    # Foreign key of user.
+    # Getting the items for a users will be easy with the related_name field
+    # If user if deleted, all of their items are also deleted
+    # Will soon import User
+    created_date = models.DateTimeField(auto_now_add=True)
