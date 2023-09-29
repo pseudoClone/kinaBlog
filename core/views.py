9ff792109c3a7e7f91578def9359d63b3da26238
pseudoclone <prince.077bei027@acem.edu.np>
2023-09-29 10:53:53 +0545

    update: updating view to show the products manually fix: fix the is_sold typo with isSold

--- core/views.py
+++ core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from item.models import Category,Item
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html') #The template is the index.html
+    items = Item.objects.filter(isSold = False)[:6] # Get the newest 6 items from the items
+    categories = Category.objects.all() # Get all the avaliable categories
+    return render(request, 'core/index.html',
+                  {'categories': categories,
+                      'items' : items,})
+                  #These names point to above variables. They must be referenced for using in template
 
 def contact(request):
     return render(request, 'core/contact.html')
+
