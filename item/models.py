9d52a6949e83b77ec29dbb153f35af278f8afb29
pseudoclone <prince.077bei027@acem.edu.np>
2023-09-29 08:54:10 +0545

    update: new image Field for item images

--- item/models.py
+++ item/models.py
@@ -16,14 +16,17 @@ class Item(models.Model):
     category = models.ForeignKey(Category,related_name='item', on_delete=models.CASCADE)
     #On deletion of category whole items in the category will be deleted
 
+    image = models.ImageField(upload_to='item_images', blank = True, null = True) # Where on the server to keep the images is upload_to
+
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True) #If user enters nothing in the description, then its fine too
+    
     price = models.FloatField()
     isSold = models.BooleanField(default=False)
+
     created_by = models.ForeignKey(User,related_name = 'items', on_delete = models.CASCADE)
     # Foreign key of user.
     # Getting the items for a users will be easy with the related_name field
     # If user if deleted, all of their items are also deleted
     # Will soon import User
-    
     created_date = models.DateTimeField(auto_now_add=True)
