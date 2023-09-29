f6ddb12d6675f8873f580f97cd86297fbbed4a5d
pseudoclone <prince.077bei027@acem.edu.np>
2023-09-29 13:44:55 +0545

    fix: try to fix typo for settings.py slash update: index.html update with categories

--- puddle/settings.py
+++ puddle/settings.py
@@ -116,7 +116,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/' #Adding the folder for media folder, automatically creates 
 MEDIA_ROOT = BASE_DIR / 'media'
