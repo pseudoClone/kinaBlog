502a53649c64fab28ba93d95303aecc4b1eefa03
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-14 13:45:23 +0545

    prune(project): prune project, facilitate new project

--- item/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'item' #Say that this is a namespace, but idk
-
-urlpatterns = [
-        path('<int:pk>/', views.detail, name = 'detail'),
-        ]
