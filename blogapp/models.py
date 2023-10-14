7bc933c3c7191849b1ab4985e60816747c084236
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-14 18:07:52 +0545

    fix(typo): fix typo TextFile to TextField

--- blogapp/models.py
+++ blogapp/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length = 255)
     author = models.ForeignKey(User, on_delete = models.CASCADE) # On deletion of the user the post are deleted too with the CASCADE option
-    body = models.TextFile()
+    body = models.TextField()
 
     def __str__(self):
         return self.title + ' | ' + self.author # This shows up in the admin page
