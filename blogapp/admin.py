0792bc4f61fb82baa58ed7352669fcdaa9f1d8f1
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-14 18:11:17 +0545

    feat(admin_model): make the model accessible to the admin area

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# models.py defines your database models. admin.py registers your models with the Django admin
+# Basically register the model in to the admin area
+from .models import Post
+
+admin.site.register(Post) # Make blog post accessible to the admin area
