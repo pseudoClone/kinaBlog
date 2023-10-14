f63bd90580f2ad45f00762578f41d45876eaa1d3
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-14 16:45:06 +0545

    feat(site): make home page by return a renderingn requesting

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html',{}) # render the request to home.html and point to a dictionary
