c0f072d6f61f3a5a2e505fb87a8ba0cd84a16c28
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-14 18:06:22 +0545

    feat(models): add attribute/fields for the defined model

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 # Now lets connect to the user that we just created(i.e admin)
-from django.contrib.auth.model import User
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length = 255)
+    author = models.ForeignKey(User, on_delete = models.CASCADE) # On deletion of the user the post are deleted too with the CASCADE option
+    body = models.TextFile()
+
+    def __str__(self):
+        return self.title + ' | ' + self.author # This shows up in the admin page
