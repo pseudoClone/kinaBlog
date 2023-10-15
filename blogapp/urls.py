10103e5e8e546045fbd19877ddd9d179306377c8
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-15 08:22:52 +0545

    feat(urls): add url path for class based view for article details

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 # from . import views
-from .views import HomeView
+from .views import HomeView,ArticleDetailView
 
 urlpatterns = [
     #path('',views.home,name='home'),
     path('',HomeView.as_view(), name="home"), #as_view returns a callable view that takes request and returns response
+    path('article/<int:pk>', ArticleDetailView.as_view(), name = "article-detail"), #int:pk will get the pk of the post
 ]
