4470c4ee2164422751f06f8aa0b5dac9368c8fba
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-15 07:43:44 +0545

    feat(views): make class based views for easy templating

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView #List in hugo listTemplates and viewTemplate
 
-# Create your views here.
+# Will be using class based views from now on
 
-def home(request):
-    return render(request, 'home.html',{}) # render the request to home.html and point to a dictionary
+#def home(request):
+#    return render(request, 'home.html',{}) # render the request to home.html and point to a dictionary
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
