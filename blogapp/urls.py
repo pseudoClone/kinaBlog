9ec582ab3a068b644c9da4385f8949a0c5b0d812
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-15 08:08:19 +0545

    feat(view): use class based view for homepage url

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import HomeView
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    #path('',views.home,name='home'),
+    path('',HomeView.as_view(), name="home"), #as_view returns a callable view that takes request and returns response
 ]
