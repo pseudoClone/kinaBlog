ba929595bc0a021139b0c2893cc20d29ac655e99
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-17 07:16:45 +0545

    feat(add_post_url): make a url for adding blog posts

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from . import views
-from .views import HomeView,ArticleDetailView
+from .views import HomeView,ArticleDetailView, AddPostView
 
 urlpatterns = [
     #path('',views.home,name='home'),
     path('',HomeView.as_view(), name="home"), #as_view returns a callable view that takes request and returns response
     path('article/<int:pk>', ArticleDetailView.as_view(), name = "article-detail"), #int:pk will get the pk of the post
+    path('add-post', AddPostView.as_view(), name = 'add_post'),
 ]
