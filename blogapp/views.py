afea5b157a86833fe409950654e2f544da7a4e05
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-18 08:22:05 +0545

    feat(form): create form fields in views

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView #List in hugo listTemplates and viewTemplate
 from .models import Post
+from .forms import PostForm
 # Will be using class based views from now on
 
 #def home(request):
@@ -16,5 +17,7 @@ class ArticleDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__' # This makes the class use all fields from the post model we created
+    #fields = '__all__' # This makes the class use all fields from the post model we created
+    # Since we use PostForm we will not need field setting
