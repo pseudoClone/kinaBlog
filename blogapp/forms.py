6f2790960a4ab1b603e92fe58f092795944bf99d
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-18 08:20:02 +0545

    feat(form): create forms.py for post creation of forms and styling

---
+++ blogapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm): #allows to create fields for the model
+    class Meta:
+        model = Post
+        fields = ('title','title_tag', 'author', 'body')
+        widgets = {
+                'title': forms.TextInput(attrs = {'class': 'form-control'}),
+                'title_tag': forms.TextInput(attrs = {'class': 'form-control'}),
+                'author': forms.Select(attrs = {'class': 'form-control'}), #Because select dropdown box is the type for author
+                'body': forms.Textarea(attrs = {'class': 'form-control'}),
+                }
