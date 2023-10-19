867783aece392195e443ebe2438385b43373ea9f
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-19 09:25:46 +0545

    feat(form_update): Make class for updating form & leaving author field Since fields and form_class cannot exist in the same scope, I am doing this.

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -11,3 +11,12 @@ class PostForm(forms.ModelForm): #allows to create fields for the model
                 'author': forms.Select(attrs = {'class': 'form-control'}), #Because select dropdown box is the type for author
                 'body': forms.Textarea(attrs = {'class': 'form-control'}),
                 }
+class EditForm(forms.ModelForm): #allows to create fields for the model
+    class Meta:
+        model = Post
+        fields = ('title','title_tag', 'body')
+        widgets = {
+                'title': forms.TextInput(attrs = {'class': 'form-control'}),
+                'title_tag': forms.TextInput(attrs = {'class': 'form-control'}),
+                'body': forms.Textarea(attrs = {'class': 'form-control'}),
+                }
