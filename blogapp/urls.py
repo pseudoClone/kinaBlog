6eef58bc96403fdb1b60fa23ce290f1d77c81c98
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-19 09:03:18 +0545

    feat(edit_page_url): Create url for editing post This will take an existing post from its url and edit it

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 # from . import views
-from .views import HomeView,ArticleDetailView, AddPostView
+from .views import HomeView,ArticleDetailView, AddPostView, UpdatePostView
 
 urlpatterns = [
     #path('',views.home,name='home'),
     path('',HomeView.as_view(), name="home"), #as_view returns a callable view that takes request and returns response
     path('article/<int:pk>', ArticleDetailView.as_view(), name = "article-detail"), #int:pk will get the pk of the post
     path('add-post', AddPostView.as_view(), name = 'add_post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name = 'edit_post'), # Edit path makes it unique rather than creating a new post
 ]
