7edc6837bdbfc2b7aa9286ff0e8bd2a51c0efca3
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-19 10:44:13 +0545

    feat(pattern_change): Change url pattern for post deletion Use <int:pk>/remove instead of remove/<int:pk>

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('article/<int:pk>', ArticleDetailView.as_view(), name = "article-detail"), #int:pk will get the pk of the post
     path('add-post', AddPostView.as_view(), name = 'add_post'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name = 'edit_post'), # Edit path makes it unique rather than creating a new post
-    path('article/<int:pk>/prune', DeletePostView.as_view(), name = 'delete_post'),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name = 'delete_post'),
 ]
