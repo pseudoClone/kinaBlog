f00cbe7cde757a9e136a58418f4ee62816038459
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-19 09:04:00 +0545

    feat(edit_view): make a view for editing post

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView #List in hugo listTemplates and viewTemplate
+from django.views.generic import ListView, DetailView, CreateView,UpdateView #List in hugo listTemplates and viewTemplate
 from .models import Post
 from .forms import PostForm
 # Will be using class based views from now on
@@ -21,3 +21,8 @@ class AddPostView(CreateView):
     template_name = 'add_post.html'
     #fields = '__all__' # This makes the class use all fields from the post model we created
     # Since we use PostForm we will not need field setting
+
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = 'editpost.html'
+    fields = ['title', 'title_tag', 'body'] # Changing author does not make sense and yes security
