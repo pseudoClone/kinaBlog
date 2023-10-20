dc1db9813997f4657deb47f989231c8328877a85
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-20 08:27:21 +0545

    feat(post_date): Add post_date field to the model Set auto date for ignoring user input date

--- blogapp/models.py
+++ blogapp/models.py
@@ -2,12 +2,14 @@ from django.db import models
 # Now lets connect to the user that we just created(i.e admin)
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length = 255)
     title_tag = models.CharField(max_length = 255)
     author = models.ForeignKey(User, on_delete = models.CASCADE) # On deletion of the user the post are deleted too with the CASCADE option
     body = models.TextField()
+    post_date = models.DateField(auto_now_add = True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author) # This shows up in the admin page
